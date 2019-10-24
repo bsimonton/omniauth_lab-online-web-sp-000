e7f9efa49b95a9d94a8f8c1c9b422bc7cf51e53c
Brian Simonton <bsimonton78@gmail.com>
2019-10-24 01:13:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
+    t.string "image"
+    t.string "uid"
   end
 
 end
